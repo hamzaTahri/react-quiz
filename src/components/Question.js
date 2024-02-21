48467193b44fed4cd5b891eb4d5fd578636f08d8
Hamza TAHRI <werty.hamza@gmail.com>
2024-02-21 13:29:43 +0100

    switch to context/provider

---
+++ src/components/Question.js
@@ -0,0 +1,15 @@
+import Options from "./Options";
+import {useQuiz} from "../context/QuizContext";
+
+function Question() {
+    const {questions, dispatch,index, answer} = useQuiz();
+    const question = questions.at(index);
+    return (
+        <div>
+            <h4>{question.question}</h4>
+            <Options question={question} dispatch={dispatch} answer={answer}/>
+        </div>
+    );
+}
+
+export default Question;
